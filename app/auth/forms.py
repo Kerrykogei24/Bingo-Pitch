8f91624a056f8cf8e2565a52ac688d2e122f2f24
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-12-07 02:18:51 +0300

    create classes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,17 +1,17 @@
-# from flask_wtf import FlaskForm
-# from wtforms import BooleanField, StringField, PasswordField,SubmitField, validators
-# from wtforms.validators import Required, Email, EqualTo, Length
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, StringField, PasswordField,SubmitField, validators
+from wtforms.validators import Required, Email, EqualTo, Length
 
-# class RegisterForm(FlaskForm):
-#     f_name = StringField('First Name', validators = [Required()])
-#     l_name = StringField('Last Name',validators = [Required()])
-#     email = StringField('Email Address', validators = [Required(), Email()])
-#     password = PasswordField('Enter Password', validators = [ Required(), Length(min=8, max=20), EqualTo('confirm', message='Passwords must match.') ])
-#     confirm = PasswordField('Confirm Password',validators = [ Required() ])
-#     submit = SubmitField('Sign Up')
+class RegisterForm(FlaskForm):
+    f_name = StringField('First Name', validators = [Required()])
+    l_name = StringField('Last Name',validators = [Required()])
+    email = StringField('Email Address', validators = [Required(), Email()])
+    password = PasswordField('Enter Password', validators = [ Required(), Length(min=8, max=20), EqualTo('confirm', message='Passwords must match.') ])
+    confirm = PasswordField('Confirm Password',validators = [ Required() ])
+    submit = SubmitField('Sign Up')
 
 
-# class LoginForm(FlaskForm):
-#     email = StringField('Email Address', validators = [Required(), Email()])
-#     password = PasswordField('Confirm Password',validators = [ Required() ])
-#     submit = SubmitField('Login')
+class LoginForm(FlaskForm):
+    email = StringField('Email Address', validators = [Required(), Email()])
+    password = PasswordField('Confirm Password',validators = [ Required() ])
+    submit = SubmitField('Login')
