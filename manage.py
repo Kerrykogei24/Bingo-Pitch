bee900e2c00c1ffdeeb1577ca08767b462a670ca
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-12-07 02:24:29 +0300

    initialize db

--- manage.py
+++ manage.py
@@ -1,21 +1,21 @@
-from flask_script import Manager,Server
-from flask_migrate import Migrate, MigrateCommand
+# from flask_script import Manager,Server
+# from flask_migrate import Migrate, MigrateCommand
 
-from app import create_app, db
-from app.models import User, Category, Pitch, Upvote, Comment
+# from app import create_app, db
+# from app.models import User, Category, Pitch, Upvote, Comment
 
-app = create_app('production')
-manager = Manager(app)
-manager.add_command('server', Server)
+# app = create_app('production')
+# manager = Manager(app)
+# manager.add_command('server', Server)
 
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
+# migrate = Migrate(app, db)
+# manager.add_command('db', MigrateCommand)
 
 
 
-@manager.shell
-def make_shell_context():
-    return dict(app = app, db = db, User = User, Category = Category, Pitch = Pitch, Upvote = Upvote, Comment = Comment)
+# @manager.shell
+# def make_shell_context():
+#     return dict(app = app, db = db, User = User, Category = Category, Pitch = Pitch, Upvote = Upvote, Comment = Comment)
 
-if __name__ == '__main__':
-    manager.run()
+# if __name__ == '__main__':
+#     manager.run()
